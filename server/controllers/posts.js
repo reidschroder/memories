b9cbb80472ef271630255f8d48589dd9c33d254f
Reid Schroder <reidschroder2@gmail.com>
2022-09-09 16:09:39 -0400

    posts.js page

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -12,16 +12,16 @@ export const getPosts = async (req, res) => {
     
 }
 
-export const createPost = (req, res) => {
+export const createPost = async (req, res) => {
     const post = req.body;
 
-    const newPost
+    const newPost = new PostMessage(post)
     try {
-        const postMessages = await PostMessage.find();
+        await newPost.save();
        
 
-        res.status(200).json(postMessages)
+        res.status(201).json(newPost)
     } catch (error) {
-        res.status(404).json({ message: error.message});
+        res.status(409).json({ message: error.message});
     }
 }
